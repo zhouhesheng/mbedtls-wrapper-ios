7d224565baf8ccdaff2ad1cc9b611970067be322
Raimundas Sakalauskas <raimis@pinstudios.com>
2018-08-14 19:50:27 +0300

    add initial project files and build phase for downloading mbedtls c lib and building for ios use (x86_64, armv7s, arm64)

---
+++ mbedTLSWrapper/mbedTLSWrapper.h
@@ -0,0 +1,19 @@
+//
+//  mbedTLSWrapper.h
+//  mbedTLSWrapper
+//
+//  Created by Raimundas Sakalauskas on 14/08/2018.
+//  Copyright © 2018 Particle Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mbedTLSWrapper.
+FOUNDATION_EXPORT double mbedTLSWrapperVersionNumber;
+
+//! Project version string for mbedTLSWrapper.
+FOUNDATION_EXPORT const unsigned char mbedTLSWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mbedTLSWrapper/PublicHeader.h>
+
+
